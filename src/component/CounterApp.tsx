81b3725c4a0e4b884bd348cf8900cab9ffaf4943
Jeevan s <aryajeevan68@gmail.com>
2025-10-07 10:17:02 +0530

    tailwind and counterapp component created

---
+++ src/component/CounterApp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CounterApp:React.FC = () => {
+  return (
+    <div className='w-[500px] h-[500px] bg-blue-950'>Counter App</div>
+  )
+}
+
+export default CounterApp;
